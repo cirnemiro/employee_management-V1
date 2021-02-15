6ab70f677637050aba68b3f0edc133c231150573
cirne <cirnemiro@gmail.com>
2021-02-15 21:31:53 +0100

    validation structure

---
+++ assets/js/validation.js
@@ -0,0 +1,40 @@
+export const validation = (value,identifier)=>{
+    console.log('im in');
+    const regExp = {
+        name : /^[A-Za-z]{2,10}/g,
+        lastName : /^[A-Za-z]{2,10}/g,
+        email : /ab+c/i,
+        postalCode : /ab+c/i,
+        
+    }
+
+    console.log(regExp[identifier]);
+   
+
+    if (value.match(regExp[identifier])) {
+        console.log('good');
+    }else{
+        console.log('nanana');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    // console.log('args from vlaidation function', args);
+    // if (args.item.name != 'klkpasa') {
+    //             console.log('updating error');
+    //             args.cancel = true
+    // }
+    // if(args.item.name){
+
+    // }
+}
+
