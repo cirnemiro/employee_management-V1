7cc2817e65594c548813704cf8b522cff80008f7
marcelarie <googlillo@gmail.com>
2021-02-16 10:36:58 +0100

    Add checkInputs method on modal component

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -24,53 +24,53 @@ export const modal = {
                 </div>
                 <div class="employee-modal__pair">
                     <label>Name</label>
-                    <input name="name" class="employee-modal-input employee-modal-input__name" id="employee-modal-input__name" value="${employee ? employee.name : ''}" ${action}></input>
+                    <input name="name" class="employee-modal-input employee-modal-input__name" id="employee-modal-input__name" value="${employee ? employee.name : ''}" required ${action}></input>
                 </div>
                 <div class="employee-modal__pair">
                     <label>Last Name</label>
-                    <input name="lastName" class="employee-modal-input" id="employee-modal-input__lastName" value="${employee ? employee.lastName : ''}" ${action} ></input>
+                    <input name="lastName" class="employee-modal-input" id="employee-modal-input__lastName" value="${employee ? employee.lastName : ''}" required ${action}></input>
                 </div>
             </div>
             <div class="employee-modal__inputs-container">
                 <div class="employee-modal__pair">
                     <label>Age</label>
-                    <input name="age" class="employee-modal-input" id="employee-modal-input__age" value="${employee ? employee.age : ''}" ${action}></input>
+                    <input name="age" class="employee-modal-input" id="employee-modal-input__age" value="${employee ? employee.age : ''}" required ${action}></input>
                 </div>
                 <div class="employee-modal__pair">
                     <label>Gender</label>
-                    <input name="gender" class="employee-modal-input" id="employee-modal-input__gender" value="${employee ? employee.gender : ''}" ${action}></input>
+                    <input name="gender" class="employee-modal-input" id="employee-modal-input__gender" value="${employee ? employee.gender : ''}" required ${action}></input>
                 </div>
                 <div class="employee-modal__pair">
                     <label>E-mail</label>
-                    <input name="email" class="employee-modal-input" id="employee-modal-input__email" value="${employee ? employee.email : ''}" ${action}></input>
+                    <input name="email" class="employee-modal-input" id="employee-modal-input__email" value="${employee ? employee.email : ''}" required ${action}></input>
                 </div>
             </div>
             <div class="employee-modal__inputs-container">
                 <div class="employee-modal__pair">
                     <label>ID</label>
-                    <input name="id" class="employee-modal-input" id="employee-modal-input__id" value="${employee ? employee.id : ''}" ${action}></input>
+                    <input name="id" class="employee-modal-input" id="employee-modal-input__id" value="${employee ? employee.id : ''}" required ${action}></input>
                 </div>
                 <div class="employee-modal__pair">
                     <label>Phone</label>
-                    <input name='phone' class="employee-modal-input" id="employee-modal-input__phoneNumber" value="${employee ? employee.phoneNumber : ''}" ${action}></input>
+                    <input name='phone' class="employee-modal-input" id="employee-modal-input__phoneNumber" value="${employee ? employee.phoneNumber : ''}" required ${action}></input>
                 </div>
                 <div class="employee-modal__pair">
                     <label>State</label>
-                    <input name="state" class="employee-modal-input" id="employee-modal-input__state" value="${employee ? employee.state : ''}" ${action}></input>
+                    <input name="state" class="employee-modal-input" id="employee-modal-input__state" value="${employee ? employee.state : ''}" required ${action}></input>
                 </div>
             </div>
             <div class="employee-modal__inputs-container">
                 <div class="employee-modal__pair">
                     <label>City</label>
-                    <input name="city" class="employee-modal-input" id="employee-modal-input__city" value="${employee ? employee.city : ''}" ${action}></input>
+                    <input name="city" class="employee-modal-input" id="employee-modal-input__city" value="${employee ? employee.city : ''}" required ${action}></input>
                 </div>
                 <div class="employee-modal__pair">
                     <label>Postal Code</label>
-                    <input name="postalCode" class="employee-modal-input" id="employee-modal-input__postalCode" value="${employee ? employee.postalCode : ''}" ${action}></input>
+                    <input name="postalCode" class="employee-modal-input" id="employee-modal-input__postalCode" value="${employee ? employee.postalCode : ''}" required ${action}></input>
                 </div>
                 <div class="employee-modal__pair">
                     <label>Street Address</label>
-                    <input name="streetAddress" class="employee-modal-input" id="employee-modal-input__streetAddress" value="${employee ? employee.streetAddress : ''}" ${action}></input>
+                    <input name="streetAddress" class="employee-modal-input" id="employee-modal-input__streetAddress" value="${employee ? employee.streetAddress : ''}" required ${action}></input>
                 </div>
             </div>
             `
@@ -78,14 +78,26 @@ export const modal = {
     },
     checkInputs: function () {
         const form = document.forms[0];
-        const inputNames = [
+        const inputNames = {
             'name', 'lastName', 'age', 'gender', 'email',
             'id', 'phone', 'state', 'city', 'postalCode', 'streetAddress'
-        ]
-        inputNames.forEach(name => {
-            console.log(form[name])
+        }
+        const regex = []
+        const result = [];
+        inputNames.forEach((name, index) => {
+            if (form[name].checkValidity()) {
+                result.push(true)
+            } else {
+                console.log('INVALID')
+                console.log(form[name])
+            }
         })
-        return true;
+
+        if (result.length === inputNames.length) {
+            return true;
+        } else {
+            return false;
+        }
     },
     getInputValues: function (data, mode) {
         const keys = Object.keys(data);
@@ -122,7 +134,6 @@ export const modal = {
                 this.modalButtonListener(data, 'enabled');
             } else {
                 if (this.checkInputs()) {
-
                     this.getInputValues(data, mode);
 
                     $('#employee-modal').remove();
